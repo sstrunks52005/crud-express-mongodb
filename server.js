20418ea84dccc0182bf010639c7f227e7c74390e
bowiewan <[bowiewan@gmail.com>
2022-06-01 23:10:22 -0500

    added ejs read and update

--- server.js
+++ server.js
@@ -4,26 +4,57 @@ const MongoClient = require('mongodb').MongoClient
 const app = express();
 const connectionString = 'mongodb+srv://bowiewan:LgkSqja1YDbMBFqX@cluster0.nx3ojnh.mongodb.net/?retryWrites=true&w=majority'
 
-MongoClient.connect(connectionString, {useUnifiedToplogy:
-true })
+MongoClient.connect(connectionString, { useUnifiedTopology: true })
     .then(client => {
         console.log('Connected to Database')
         const db = client.db('star-wars-quotes')
+        const quotesCollection = db.collection('quotes')   //collection to store items in
+
+        app.set('view engine', 'ejs')  //tells express ejs is the template engine to render html
 
         app.use(bodyParser.urlencoded({ extended: true }))
+        app.use(express.static('public'))       //tell express to make public folder accessible to public
+        app.use(bodyParser.json())
 
-        app.get('/', function (req,res){
-            res.sendFile(__dirname + '/index.html')
+        app.get('/', (req,res) => {
+            quotesCollection.find().toArray()
+                .then(results => {
+                    console.log(results)
+                    res.render('index.ejs', { quotes: results })
+                })
+                .catch(error => console.error(error))   
         })
 
         app.post('/quotes', (req, res) =>{
-            console.log(req.body)
+            quotesCollection.insertOne(req.body)
+                .then(result => {
+                    res.redirect('/')
+                })
+            .catch(error => console.error(error))
         })
+
+        app.put('/quotes', (req, res) => {
+            quotesCollection.findOneAndUpdate(
+                { name: 'yoda' },
+                {
+                  $set: {
+                    name: req.body.name,
+                    quote: req.body.quote
+                  }
+                },
+                {
+                  upsert: true
+                }
+              )
+                .then(result => {
+                    console.log(result)
+                })
+                .catch(error => console.error(error))
+          })
 
         app.listen(3000, function() {
             console.log('listening on 3000')
         })
-
     })
     .catch(error => console.error(error))
 
